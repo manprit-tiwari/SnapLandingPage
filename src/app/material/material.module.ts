22022c07b2c097b84e6559aa15c681f5302a3b8b
Manprit Tiwari <tiwarimanprit1@gmail.com>
2022-06-19 18:46:01 +0530

    Desktop version and mobile version completed

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,7 +5,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
-import {MatRippleModule} from '@angular/material/core';
+import { MatRippleModule } from '@angular/material/core';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 const Materials = [
@@ -14,7 +15,8 @@ const Materials = [
   MatSidenavModule,
   MatExpansionModule,
   MatListModule,
-  MatRippleModule
+  MatRippleModule,
+  MatMenuModule
 ]
 @NgModule({
   imports: [Materials],
