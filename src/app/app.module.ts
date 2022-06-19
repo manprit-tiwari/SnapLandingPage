726f3c803e0fc1238ef7160895e4695b18c68a52
Manprit Tiwari <tiwarimanprit1@gmail.com>
2022-06-19 14:17:53 +0530

    both mobile and desktop view layout completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './Navigation/nav-bar/nav-bar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
