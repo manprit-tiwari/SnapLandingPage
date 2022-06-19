31d62f8cf87e99aeb001963ab8297d471ad9a7c6
Manprit Tiwari <tiwarimanprit1@gmail.com>
2022-06-19 17:51:29 +0530

    Mobile Side Nav Bar completed

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import {MatRippleModule} from '@angular/material/core';
+
 
 const Materials = [
   MatButtonModule,
   MatIconModule,
-  MatSidenavModule
+  MatSidenavModule,
+  MatExpansionModule,
+  MatListModule,
+  MatRippleModule
 ]
 @NgModule({
   imports: [Materials],
